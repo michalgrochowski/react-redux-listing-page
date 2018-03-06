66127ba92009cba5c76161d5f111826ed0564788
Michał Grochowski <mic.grochowski@gmail.com>
2018-03-06 14:35:54 +0100

    v0.4.7
    Some more styling and photos

--- src/components/ProductBox.js
+++ src/components/ProductBox.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const ProductBox = (props) => {
-
+  console.log(props.photo);
   const price = props.data.map( variant => {
     if (variant.isSelected === true) {
       return (
